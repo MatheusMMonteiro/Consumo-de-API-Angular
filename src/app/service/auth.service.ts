2f4162489660d42bdbbffc359c56f3dcbaf91c62
Matheus <matheus.malmeida7@gmail.com>
2021-11-07 20:03:52 -0300

    Componente inicio sem consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserLogin } from '../model/UserLogin';
 import { User } from '../model/User';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +19,17 @@ export class AuthService {
   }
 
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('https://blogriqueov.herokuapp.com/usuarios/cadastrar', user)
+    return this.http.post<User>('https://projetinhoblogpessoal.herokuapp.com/usuarios/cadastrar', user)
 
+  }
+
+  logado(){
+    let ok = false
+
+    if(environment.token != ''){
+      ok = true
+    }
+
+    return ok
   }
 }
